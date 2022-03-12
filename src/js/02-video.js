0adc583e51d782b03a0574a18c7519a5f8d43048
wojciechonoszko <wojciechonoszko@gmail.com>
2022-03-12 22:28:53 +0100

    updating

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,69 @@
+//const throttle = require('lodash.throttle');
+// const _ = require('lodash');
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
 
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+const add = _.add(5,6);
+console.log(add);    
+const myFn = () => {
+    console.log('test');
+}
+
+const myNewFn = _.throttle(() => {
+    console.log('test');
+}, 300);
+
+
+const onPlay = function(data) {
+    const currentTime = player.getCurrentTime().then(function(currentTime) { 
+    //console.log(currentTime);
+    const currentTimeString = currentTime.toString();
+    // console.log(currentTimeString);
+    localStorage.setItem("videoplayer-current-time", currentTimeString);
+    const savedTime = localStorage.getItem("videoplayer-current-time");
+     
+    // console.log(savedTime);
+        // duration = the duration of the video in seconds
+    }).catch(function(error) {
+        // an error occurred
+    });
+
+   // data is an object containing properties specific to that event
+};
+    
+//player.on('play', onPlay);
+player.on('timeupdate', _.throttle(onPlay, 5000))
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
+
+
+
+
+
+
+    //  player.ready().then(() => {
+    //   player.getDuration().then()((data) => console.log(data));
+    // }).catch((err) => console.log(err));
+  
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+const form = document.querySelector("form");
+
+console.log(form);
+
+
+
+inputElementsUpdate();
+
+
+
+form.addEventListener("input", saveMessage);
+form.addEventListener("submit", resetForm);
+
+function saveMessage(evt) {
+    evt.preventDefault();
+    const obj = {
+        email: document.querySelector("input").value,
+        message: document.querySelector("textarea").value,
+    };
+    const storage = localStorage.setItem("feedback-form-state", JSON.stringify(obj));
+    inputElementsUpdate();
+    //form.reset();
+
+};
+
+function resetForm() {
+    form.reset();
+}
+
+function inputElementsUpdate() {
+    const storageItem = localStorage.getItem("feedback-form-state");
+    const parsedStorage = JSON.parse(storageItem);
+    document.querySelector("input").value = parsedStorage.email;
+    document.querySelector("textarea").value = parsedStorage.message;
+    console.log(parsedStorage);
+}
+
+
+
 
